f887464c671b22f8d80015c12e1e0b1e84b19527
Joseph Fenly <joseph@fenly.co.uk>
2017-01-24 17:21:00 +0000

    Work on Driver Data capture

--- Invoke-QuickImport.ps1
+++ Invoke-QuickImport.ps1
@@ -6,16 +6,10 @@
     - Move source to set standard location, create folder structure if required
     - Import into SCCM
     - 
-.PARAMETER
-
-.PARAMETER
-
-.EXAMPLE
-
 .NOTES
-    FileName:   Invoke-QuickImportCli
+    FileName:   Invoke-QuickImportCli.ps1
     Author:     Joseph Fenly
-    Created:    2017-01-19
+    Created:    19-01-2017
     Version:    1.0
 
     Change Log: 1.0 - Initial Commit
@@ -23,32 +17,23 @@
 $siteServer = "dur-vmsccm-03.waterstons.local"
 $drvRoot = "\\dur-vmfp-02\SCCM_Package_Sources\Driver_Source"
 $pkgRoot = "\\dur-vmfp-02\SCCM_Package_Sources\Packages"
-Begin{
-    function get-connect{
-	Write-Host "INFO: Establishing connection to CM Site Server" -ForegroundColor Yellow
-	try{
-	    Test-Connection $siteServer -ea Stop | Out-Null
-	    Write-Host "INFO: Connection successfully established" -Foreground Yellow
-	}
-	catch{
-	    Write-Host "ERROR: Connection could not be established" -ForegroundColor Red
-	}
+function get-cmconnect{
+    Write-Host "INFO: Establishing connection to CM Site Server" -ForegroundColor Yellow
+    try{
+	Test-Connection $siteServer -ea Stop | Out-Null
+	Write-Host "INFO: Connection successfully established" -Foreground Yellow
     }
-    function get-drvsrc{
-	Write-Host "INFO: Gathering Driver Information, please provide the following"
-	$dvr = Read-Host "Device Vendor eg. HP or DELL"
-	$dmd = Read-Host "Device Model eg. Elitebook 840 G3"
-	$dos = Read-Host "Operating System"
-	$dac = Read-Host "x64 or x86"
-	Write-Host "INFO: Driver Information stored"
-    if(Test)
+    catch{
+	Write-Host "ERROR: Connection could not be established" -ForegroundColor Red
     }
-
 }
-Process{
-
+function get-drvdata{
+    Write-Host "INFO: Gathering Driver Information, please provide the following"
+    $dvr = Read-Host "Device Vendor eg. HP or DELL"
+    $dmd = Read-Host "Device Model eg. Elitebook 840 G3"
+    $dos = Read-Host "Operating System"
+    $dac = Read-Host "x64 or x86"
+    $src = Read-Host "Local Driver Source"
+    Write-Host "INFO: Driver Information stored"
+    # $script:drvdata =
 }
-End{
-
-}
- 
